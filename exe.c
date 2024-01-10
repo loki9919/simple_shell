db218031e94e754b779a0825258c2432708a085a
lokmane rouijel <lokmane9919@gmail.com>
2024-01-10 15:16:25 -0800

    simple shell

--- exe.c
+++ exe.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int exec(char **order, char **con, char **argv)
+int exec(char **order, char **con, char **argv) 
 {
 	int state;
 	pid_t subprocess;
@@ -8,18 +8,21 @@ int exec(char **order, char **con, char **argv)
 	subprocess = fork();
 	if (subprocess == 0)
 	{
-
 		if (execve(order[0], order, con) == -1)
 		{
 			perror(argv[0]);
 			free_string(order);
-			exit(EXIT_FAILURE);
+			exit(127);
 		}
+	}
+	else if (subprocess > 0)
+	{
+		wait(&state);
+		free_string(order);
 	}
 	else
 	{
-		waitpid(subprocess, &state, 0);
-		free_string(order);
+		perror(argv[0]);
 	}
-	return(WEXITSTATUS(state));
+	return (WEXITSTATUS(state));
 }
